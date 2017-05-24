d14c2499cf9c8a421f2b07e5717971d76dee91ec
Zhihao <18321118675@163.com>
2017-05-24 19:58:34 +0800

    myTeamv1_05191958

--- myTeamv1.py
+++ myTeamv1.py
@@ -56,7 +56,9 @@ class BasicNode:
         return 0
 
 class StateNode( BasicNode):
-	def __init__( self, allies = None, enemies = None, GameState = None, AlliesActions = dict(), OpponentsActions = dict(), AlliesActionParent = None, OpponentsActionParent = None, StateParent = None, getDistancer = None, getDistanceDict = None ):      	        
+    def __init__( self, allies = None, enemies = None, GameState = None, AlliesActions = dict(),\
+                      EnemiesActions = dict(), AlliesActionNodeParent = None, EnemiesActionNodeParent = None,\
+                      StateParent = None, getDistancer = None, getDistanceDict = None ):      	        
         # check the format of input data!
         if StateParent is None and GameState is None:
 			raise Exception( "GameState and StateParent can not be 'None' at the same time!" )
@@ -77,7 +79,7 @@ class StateNode( BasicNode):
             raise Exception( " the format of AlliesActions and OpponentsAction go wrong!" )	
         self.StateParent = StateParent
         self.AlliesActionParent = AlliesActionParent
-        self.OpponentsActionParent = OpponentsActionParent   
+        self.EnemiesActionParent = OpponentsActionParent   
         if StateParent is None:     
             if getDistancer is None or allies is None or enemies is None:
                 raise Exception( "the function of getDistancer or allies or enemies missing!")
@@ -106,12 +108,13 @@ class StateNode( BasicNode):
             self.IndexPositions[ index ] = self.GameState.getAgentState( index ).getPosition() 
         # combine different actions for different indexes
         self.LegalAlliesActions = tuple( itertools.product( self.LegalIndexActions(self.allies[0]), self.LegalIndexActions(self.allies[1]) ) )    
-        self.LegalEnemiesActions = tuple(itertools.product( self.LegalIndexActions(self.enemies[0]), self.LegalIndexActions(self.enemies[1]) ) ) 
+        self.LegalEnemiesActions = tuple( itertools.product( self.LegalIndexActions(self.enemies[0]), self.LegalIndexActions(self.enemies[1]) ) ) 
+        self.LegalActions = tuple( itertools.product( self.LegalAlliesActions, self.LegalEnemiesActions ) )
         # self.LegalActions = self.LegalAlliesActions + self.LegalEnemiesActions
         # the following attributes 
-        self.AlliesSuccActionsNode = dict()
-  	self.EnemiesSuccActionsNode = dict()
-	self.SuccStateNode = dict()	
+        self.AlliesSuccActionsNodeDict = dict()
+  	self.EnemiesSuccActionsNodeDict = dict()
+	self.SuccStateNodeDict = dict()	
         self.nVisit = 0.0
         self.totalValue = 0.0
         self.C1 = math.sqrt( 2 )
@@ -122,62 +125,109 @@ class StateNode( BasicNode):
     def getBestActions( self ):
 	return 0        	 
 
-    def isPruned( self ):
-        """
-        isPrune is related to the self.AlliesActionParent and self.OpponentsActionParent, 
-        the return value is True or False.
-        """
-	if AlliesActionParent
-	    return True	       
+    """
+    isPrune is related to the self.AlliesActionParent and self.OpponentsActionParent, 
+    the return value is True or False.
+    """
+    def isNovel( self ):
+        if self.AlliesActionParent.novel is False or self.EnemiesActionParent.novel is False:
+            self.novel = False
+        else:
+            self.novel = True
+        return self.novel	       
 
     def isFullExpand( self ):
-		return 0
-
-    def RandGenerateActions( self ):
-        if self.isFullExpand():
-            raise Exception( " This Node has been full Expanded, you should choose UCB1" )
+        if len( self.SuccStateNodeDict ) < len( self.LegalActions ):
+            self.FullExpand = False
         else:
-            if len( self.LegalAlliesActions ) == len( self.AlliesSuccActionsNode.keys() ):
-                ChosedAlliesAction = radom.choice( self.LegalAlliesActions )
-            else:    
-                PreparedAlliesAcitons = []
-                for alliesAction in self.LegalAlliesActions:
-                   if self.AlliesSuccActionsNode.get( alliesAction ) is None:
-                       PrepareAlliesActions.append( alliesAction ) 
-                ChosedAlliesAction = random.choice( PreparedAlliesActions )
-                AlliesActionNode = ActionNode( self.allies, self.enemies, ChosedAction, self )
-                self.AlliesSuccActions is 
-              
-            if self.  
-            PreparedEnemoesActions = []
-            for enemiesAction in self.LegalEnemiesActions:
-                if self.EnemiesSuccActionsNode.get( enemoesAction ) is None:
-                    PrepareEnemiesActions.append( enemiesAction )
+            self.FullExpand = True
+	return self.FullExpand
 
+    # RandChooseLeftActions is applied in MCTS select
+    def RandChooseLeftActions( self ):
+        if self.isFullExpand():
+            raise Exception( " This Node has been full Expanded, you should choose UCB1ChooseActions!" )
+        else:
+            # Choose the action that has not been taken
+            PreparedActions =  []
+            for Action in self.LegalActions:
+                if self.SuccStateNodeDict.get( Action ) is None:
+                    PreparedActions.append( Action )
+            ChosedActions = random.choice( PreparedActions )
+            # Get the corresponding AlliesActionNode and EnemiesActionNode
+            ChosedAlliesActions, ChosedEnemiesActions = ChosedActions
+            AlliesActionNode = self.AlliesSuccActionsNodeDict.get( ChosedAlliesAction )            
+            if AlliesActionNode is None:
+                AlliesActionNode = ActionNode( self.allies, self.enemies, ChosedAlliesAction, self )
+            EnemiesActionNode = self.EnemiesSuccActionsNodeDict.get( ChosedEnemiesAction )
+            if EnemiesActionNode is None:
+                EnemiesActionNode = ActionNode( self.enemies, self.allies, ChosedEnemiesAction, self )
+            """
+            The format of AlliesActionNode and EnenmiesAcrionNode should be dict instead of list!
+            """
+            AlliesActions = dict( zip( self.allies, ChosedAlliesActions ) )
+            EnemiesActions = dict( zip( self.enemies, ChosedEnemiesActions ) )
+            SuccStateNode = StateNode( AlliesActions = AlliesActions, EnemiesActions = EnemiesActions,\
+                            AlliesActionNodeParent = AlliesActionNode, EnenmiesActionNodeParent = EnemiesActionNode, StateParent = self )
+            self.SuccStateNodeDict[ ChosedActions ] = SuccStateNode
+            return SuccStateNode     
         
-
+    # UCB1ChooseSuccNode is applied in MCTS select    
     def UCB1ChooseSuccNode( self ):
-	return 0  
-   
-    # GenerateSuccActionNode is used in 
-    def GenerateSuccActionNode( self , actions ):
-	return 0
+        if not self.isFullExpand():
+            raise Exception( "This Node has not been full expanded, you should choose RandChooseLeftActions!")
+        else:
+            HighestScore = 0
+            ChosedAlliesAction = None
+            for AlliesAction in self.LegalAlliesActions:
+                AlliesSuccActionNode = self.AlliesSuccActionsNodeDict.get( AlliesAction )
+                score = AlliesSuccActionNode.totalValue / float( AlliesSuccActionNode.nVisit ) + \
+                        self.C1 * math.sqrt( math.log( self.nVisit ) / AlliesSuccActionNode.nVisit )
+                if score >= HighestScore:
+                    HighestScore = score
+                    ChosedAlliesAction = AlliesAction
+
+            HighestScore = 0
+            ChosedEnemiesAction = None           
+            for EnemiesAction in self.LegalEnemiesActions:
+                EnemiesSuccActionNode = self.EnemiesSuccActionNodeDict.get( EnemiesAction )
+                score = - EnemiesSuccActionNode .totalValue / float( EnemiesSuccActionNode.nVisit ) + \
+                        self.C1 * math.sqrt( math.log( self.nVisit) / EnemiesSuccActionNode.nVisit )
+                if score >= HighestScore:
+                    HighestScore = score
+                    ChosedEnemiesAction = EnemiesAction
+
+             ChosedAction = tuple( ChosedAlliesAction, ChosedEnemiesAction )
+             SuccStateNode = self.SuccStateNodeDict[ ChosedAction ]  
+             return SuccStateNode       
     
-    """
-    RandGenerateSuccStateNode function is used in the condition when the node is not fully Expand, which means some certain action is still not be 
-    taken that the corresponding child node has not been built. 
-    """
-    def RandGenerateSuccStateNode( self ):
-	return 0
-    
-    """
-    RandChooseSuccNode is used in the course of MCTS's playout,  
-    """
-    def RandChooseSuccNode( self ):
-	return 0
+    # RandChooseSuccNode is used in the course of MCTS's playout      
+    def ChooseSuccNode( self, actions = None ):
+        if actions is None:
+            ChosedActions = random.choice( self.LegalActions )
+        else:
+            ChosedActions = actions
+        # Get the corresponding AlliesActionNode and EnemiesActionNode
+        ChosedAlliesActions, ChosedEnemiesActions = ChosedActions
+        AlliesActionNode = self.AlliesSuccActionsNodeDict.get( ChosedAlliesAction )            
+        if AlliesActionNode is None:
+            AlliesActionNode = ActionNode( self.allies, self.enemies, ChosedAlliesAction, self )
+        EnemiesActionNode = self.EnemiesSuccActionsNodeDict.get( ChosedEnemiesAction )
+        if EnemiesActionNode is None:
+            EnemiesActionNode = ActionNode( self.enemies, self.allies, ChosedEnemiesAction, self )
+        """
+        The format of AlliesActionNode and EnenmiesAcrionNode should be dict instead of list!
+         """
+        AlliesActions = dict( zip( self.allies, ChosedAlliesActions ) )
+        EnemiesActions = dict( zip( self.enemies, ChosedEnemiesActions ) )
+        SuccStateNode = StateNode( AlliesActions = AlliesActions, EnemiesActions = EnemiesActions,\
+                        AlliesActionNodeParent = AlliesActionNode, EnenmiesActionNodeParent = EnemiesActionNode, StateParent = self )
+        self.SuccStateNodeDict[ ChosedActions ] = SuccStateNode
+
+	return SuccStateNode
 
     """
-    The following method is used to compute the in the process of 
+    The following method is used to compute the LatentScore the process of playout when the final score has no change with the original one!
  
     getBound is used to scale the features value to interval [ 0, 0.5 ], and we name the final score as LatentScore!
     getFeatures is used to compute the features in 
@@ -254,30 +304,30 @@ class ActionNode( BasicNode, ):
 
 
 class SimulateAgent:
-	def __init__( self ):
+    def __init__( self ):
     	return 0 
 
-	def chooseAction(self, gameState):
-		return 0
+    def chooseAction(self, gameState):
+    	return 0
 
-	def getSuccessor(self, gameState, action):
-		return 0
+    def getSuccessor(self, gameState, action):
+        return 0
 
-	def evaluate(self, gameState, action):
-		return 0
+    def evaluate(self, gameState, action):
+        return 0
 
-	def getOffensiveFeatures(self, gameState, action):
-		return 0
+    def getOffensiveFeatures(self, gameState, action):
+        return 0
 
-	def getDefensiveFeatures(self, gameState, action):
-		return 0
+    def getDefensiveFeatures(self, gameState, action):
+        return 0
 
-	def getWeights(self, gameState, action):
-		return 0
+    def getWeights(self, gameState, action):
+        return 0
 
 class Distancer:
-	def __init__( self ):
-		return 0
+    def __init__( self ):
+        return 0
 
 class MCTSCaptureAgent(CaptureAgent):
         
@@ -292,11 +342,12 @@ class MCTSCaptureAgent(CaptureAgent):
 
   def chooseAction( self, GameState ):
     """ 
-    we return the best a ction for current GameState. 
+    we return the best action for current GameState. 
     we hope to return the best two action for two pacman! 
     """    
     start = time.time()
-    self.rootNode = ExploreNode( GameState, self.allies, None)
+    self.rootNode = ExploreNode( GameState, self.allies, None, allies = self.allies, enemies = self.enemies, GameState = GameState, \
+                                 getDistancer = self.getMazeDistance, getDistanceDict = None )
     node = self.rootNode
     iters = 0
     running_time = 0.0
