1a011bef1663db02feb32aad693d62e9c5a05db2
WangsyGit <siyuan.wang@transwarp.io>
2017-06-13 13:12:08 +0800

    pull

--- capture.py
+++ capture.py
@@ -1050,10 +1050,14 @@ if __name__ == '__main__':
     See the usage string for more details.
     > python capture.py --help
     """
-
+    print sys.argv[1:]
     options = readCommand(sys.argv[1:])  # Get game components based on input
+    print options
     games = runGames(**options)
+    games2 = runGames(**options)
 
     save_score(games[0])
+    print games[0].state.data.score
+    print games2[0].state.data.score
     # import cProfile
     # cProfile.run('runGames( **options )', 'profile')
